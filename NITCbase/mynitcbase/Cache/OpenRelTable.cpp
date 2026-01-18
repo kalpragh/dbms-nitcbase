b7dea96a9f282fd9fcd23c013b407f6052ebc3d4
kalpragh <kalpanaraghuram96@gmail.com>
2026-01-18 10:22:17 +0530

    updated openreltable.cpp for constructor and destructor

--- NITCbase/mynitcbase/Cache/OpenRelTable.cpp
+++ NITCbase/mynitcbase/Cache/OpenRelTable.cpp
@@ -1,3 +1,120 @@
 #include "OpenRelTable.h"
-
+#include <cstdlib>   // malloc, free
 #include <cstring>
+
+// Constructor: load RELCAT + ATTRCAT entries into caches
+OpenRelTable::OpenRelTable() {
+
+  // 1) initialize relCache and attrCache with nullptr
+  for (int i = 0; i < MAX_OPEN; ++i) {
+    RelCacheTable::relCache[i] = nullptr;
+    AttrCacheTable::attrCache[i] = nullptr;
+  }
+
+  RecBuffer relCatBlock(RELCAT_BLOCK);
+
+  Attribute relCatRecord[RELCAT_NO_ATTRS];
+  relCatBlock.getRecord(relCatRecord, RELCAT_SLOTNUM_FOR_RELCAT);
+
+  RelCacheEntry relCacheEntry0;
+  RelCacheTable::recordToRelCatEntry(relCatRecord, &relCacheEntry0.relCatEntry);
+  relCacheEntry0.recId.block = RELCAT_BLOCK;
+  relCacheEntry0.recId.slot  = RELCAT_SLOTNUM_FOR_RELCAT;
+
+  RelCacheTable::relCache[RELCAT_RELID] =
+      (RelCacheEntry*)malloc(sizeof(RelCacheEntry));
+  *(RelCacheTable::relCache[RELCAT_RELID]) = relCacheEntry0;
+
+  // ---------------------------
+  // ATTRCAT relation (relId = 1)
+  // ---------------------------
+  Attribute attrRelRecord[RELCAT_NO_ATTRS];
+  relCatBlock.getRecord(attrRelRecord, RELCAT_SLOTNUM_FOR_ATTRCAT);
+
+  RelCacheEntry relCacheEntry1;
+  RelCacheTable::recordToRelCatEntry(attrRelRecord, &relCacheEntry1.relCatEntry);
+  relCacheEntry1.recId.block = RELCAT_BLOCK;
+  relCacheEntry1.recId.slot  = RELCAT_SLOTNUM_FOR_ATTRCAT;
+
+  RelCacheTable::relCache[ATTRCAT_RELID] =
+      (RelCacheEntry*)malloc(sizeof(RelCacheEntry));
+  *(RelCacheTable::relCache[ATTRCAT_RELID]) = relCacheEntry1;
+
+  /************ Setting up Attribute cache entries ************/
+  RecBuffer attrCatBlock(ATTRCAT_BLOCK);
+
+  Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+  // ---------------------------------------------------------
+  // Attributes of RELCAT (usually slots 0 to 5 in ATTRCAT)
+  // ---------------------------------------------------------
+  AttrCacheEntry* headRel = nullptr;
+  AttrCacheEntry* tailRel = nullptr;
+
+  for (int slot = 0; slot <= 5; slot++) {
+    attrCatBlock.getRecord(attrCatRecord, slot);
+
+    AttrCacheEntry* node = (AttrCacheEntry*)malloc(sizeof(AttrCacheEntry));
+    AttrCacheTable::recordToAttrCatEntry(attrCatRecord, &node->attrCatEntry);
+
+    node->recId.block = ATTRCAT_BLOCK;
+    node->recId.slot  = slot;
+    node->next = nullptr;
+
+    if (headRel == nullptr) {
+      headRel = tailRel = node;
+    } else {
+      tailRel->next = node;
+      tailRel = node;
+    }
+  }
+
+  AttrCacheTable::attrCache[RELCAT_RELID] = headRel;
+
+  // ---------------------------------------------------------
+  // Attributes of ATTRCAT (usually slots 6 to 11 in ATTRCAT)
+  // ---------------------------------------------------------
+  AttrCacheEntry* headAttr = nullptr;
+  AttrCacheEntry* tailAttr = nullptr;
+
+  for (int slot = 6; slot <= 11; slot++) {
+    attrCatBlock.getRecord(attrCatRecord, slot);
+
+    AttrCacheEntry* node = (AttrCacheEntry*)malloc(sizeof(AttrCacheEntry));
+    AttrCacheTable::recordToAttrCatEntry(attrCatRecord, &node->attrCatEntry);
+
+    node->recId.block = ATTRCAT_BLOCK;
+    node->recId.slot  = slot;
+    node->next = nullptr;
+
+    if (headAttr == nullptr) {
+      headAttr = tailAttr = node;
+    } else {
+      tailAttr->next = node;
+      tailAttr = node;
+    }
+  }
+
+  AttrCacheTable::attrCache[ATTRCAT_RELID] = headAttr;
+}
+
+OpenRelTable::~OpenRelTable() {
+  // free relation cache entries
+  for (int i = 0; i < MAX_OPEN; i++) {
+    if (RelCacheTable::relCache[i] != nullptr) {
+      free(RelCacheTable::relCache[i]);
+      RelCacheTable::relCache[i] = nullptr;
+    }
+  }
+
+  // free attribute cache linked lists
+  for (int i = 0; i < MAX_OPEN; i++) {
+    AttrCacheEntry* cur = AttrCacheTable::attrCache[i];
+    while (cur != nullptr) {
+      AttrCacheEntry* nxt = cur->next;
+      free(cur);
+      cur = nxt;
+    }
+    AttrCacheTable::attrCache[i] = nullptr;
+  }
+}
