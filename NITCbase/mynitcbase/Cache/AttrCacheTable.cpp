c74b554c1a4b728f7e1e0626548b66148af751b2
kalpragh <kalpanaraghuram96@gmail.com>
2026-01-20 15:19:16 +0530

    edited getattrcatentry to match the attr name instead of attr offset

--- NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
+++ NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
@@ -1,36 +1,24 @@
 #include "AttrCacheTable.h"
-
 #include <cstring>
 AttrCacheEntry* AttrCacheTable::attrCache[MAX_OPEN];
 
-/* returns the attrOffset-th attribute for the relation corresponding to relId
-NOTE: this function expects the caller to allocate memory for `*attrCatBuf`
-*/
-int AttrCacheTable::getAttrCatEntry(int relId, int attrOffset, AttrCatEntry* attrCatBuf) {
-  // check if 0 <= relId < MAX_OPEN and return E_OUTOFBOUND otherwise
+int AttrCacheTable::getAttrCatEntry(int relId, char attrName[ATTR_SIZE], AttrCatEntry* attrCatBuf) {
   if(relId<0|| relId>=MAX_OPEN){
     return E_OUTOFBOUND;
   }
-  // check if attrCache[relId] == nullptr and return E_RELNOTOPEN if true
   if(attrCache[relId]==nullptr){
     return E_RELNOTOPEN;
   }
-  // traverse the linked list of attribute cache entries
   for (AttrCacheEntry* entry = attrCache[relId]; entry != nullptr; entry = entry->next) {
-    if (entry->attrCatEntry.offset == attrOffset) {
+    if (strcmp(entry->attrCatEntry.attrName,attrName)==0) {
         *attrCatBuf=entry->attrCatEntry;
         return SUCCESS;
     }
   }
 
-  // there is no attribute at this offset
   return E_ATTRNOTEXIST;
 }
 
-/* Converts a attribute catalog record to AttrCatEntry struct
-    We get the record as Attribute[] from the BlockBuffer.getRecord() function.
-    This function will convert that to a struct AttrCatEntry type.
-*/
 void AttrCacheTable::recordToAttrCatEntry(union Attribute record[ATTRCAT_NO_ATTRS],AttrCatEntry* attrCatEntry) {
   strcpy(attrCatEntry->relName, record[ATTRCAT_REL_NAME_INDEX].sVal);
   strcpy(attrCatEntry->attrName, record[ATTRCAT_ATTR_NAME_INDEX].sVal);
